19c2c6782fa7f16346b6a855986557cda4cdede5
Donis Sousa <donisgtr@gmail.com>
2024-09-17 09:38:28 -0300

    setimo

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -61,18 +61,11 @@ const scrapeLogic = async (res) => {
         await page.click("#ext-137");
         await page.type("#ext-137", "Public@99");
 
-         /*
-        // Clicando no botão de login
-        await page.waitForSelector("#ext-139");
-        await page.click("#ext-139");
-
-        try {
-          await page.waitForNavigation({ timeout: 30000, waitUntil: "networkidle2" });
-        } catch (error) {
-          console.error("Erro de navegação:", error.message);
-        }
         
-        */
+        // Clicando no botão de login
+        // await page.waitForSelector("#ext-139");
+        // await page.click("#ext-139");
+
 
         // Capturando o screenshot
         const screenshotBuffer = await page.screenshot();
